f6e88784d16d0b2c6f4684afaed98e4546ab3d7d
George <George.Bullis@superjoy.org>
2010-06-05 23:04:50 -0700

    refactored roman.py

--- roman.py
+++ roman.py
@@ -1,49 +1,31 @@
 #!/usr/bin/python 
 
-#print out roman numbers from 1 1000 - no zero required
-
-
-def pattern( pat ):
-    one, five, ten = list( pat )
-    yield one
-    yield one * 2 
-    yield one * 3
-    yield one + five
-    yield five
-    yield five + one
-    yield five +  one * 2 
-    yield five +  one * 3
-    yield one + ten
-
-
+#print out roman numbers from 1 3999 - no zero required
 #patlist = [ "IVX" , "XLC", "CDM" ]
 
+def pattern( pat ):
+    I, V, X = list( pat )
+    yield ''
+    yield I
+    yield I + I 
+    yield I + I + I
+    yield I + V
+    yield V
+    yield V + I
+    yield V + I + I
+    yield V + I + I + I
+    yield I + X
 
-
-
+def pattern_with_prefix( prefix, pat ):
+    for i in pattern( pat ):
+        yield prefix + i
+        
+def one_to_a_thousand( prefix ):
+    for j in pattern_with_prefix( prefix, "XLC" ):
+        for i in pattern_with_prefix( j, "IVX" ):
+            print  i
+    
 for m in xrange(4):
-        ms = 'M'*m
-        print ms
-
-	for i in pattern( "IVX" ):
-	    print  i
-	
-	for j in pattern( "XLC" ):
-	    print j
-	    for i in pattern( "IVX" ):
-	        print  j+i
-	
-	for k in pattern( "CDM" ):
-            mk = ms + k
-	    print mk
-	
-	    for i in pattern( "IVX" ):
-	        print  mk + i
-	
-	    for j in pattern( "XLC" ):
-                mj = mk + j
-	        print mj
-	        for i in pattern( "IVX" ):
-	            print  mj +i
-	
-
+    for k in pattern_with_prefix( 'M'*m, "CDM" ):
+        one_to_a_thousand( k )
+        
