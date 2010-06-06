fd9f58cd8e011d8bcedf7b987450180bda72f2cc
George <George.Bullis@superjoy.org>
2010-06-05 23:30:33 -0700

    that's it - no more refactoring :)

--- roman.py
+++ roman.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python 
 
 #print out roman numbers from 1 3999 - no zero required
-#patlist = [ "IVX" , "XLC", "CDM" ]
 
 def pattern( pat ):
     I, V, X = list( pat )
@@ -16,6 +15,9 @@ def pattern( pat ):
     yield V + I + I + I
     yield I + X
 
+# ---------------------------------------        
+# Simple case of 3.3 patterns
+
 def PrefixPattern( prefix, pat ):
     for i in pattern( pat ):
         yield prefix + i
@@ -26,4 +28,21 @@ for T in xrange(4):
             for I in PrefixPattern( C, "IVX" ):
                 print  I
 
+# ---------------------------------------        
+# Extra Credit 
+# General case that can handle N patterns
+def PrefixPattern2( prefix, patlist ):
+    for i in pattern( patlist[0] ):
+        if len( patlist ) == 1:
+            yield prefix + i
+        else:
+            for j in PrefixPattern2( prefix + i, patlist[1:] ):
+                yield j
+
+def Call_PrefixPattern2_Thrice():
+    Patlist = [ "CDM" , "XLC", "IVX"   ]
+    for T in xrange(4):
+        for i in PrefixPattern2( 'M'*T, Patlist ):
+            print i
+    
         
