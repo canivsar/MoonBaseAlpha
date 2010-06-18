f6ac17ce4fc6dec1f47d58b306121d2ee6708473
George <George.Bullis@superjoy.org>
2010-06-18 07:31:32 -0700

    first render of example - not clear on Forward vs backward

--- circuts/decoder.py
+++ circuts/decoder.py
@@ -31,6 +31,8 @@ circut = "".join( circut.split() )
     #     print gate,inputs, "no match"
 
 
+print "digraph G {"
+
 class cgate:
     all = []
     count = 0
@@ -40,28 +42,36 @@ class cgate:
         self.id = cgate.count
         cgate.count +=  1
         cgate.all.append( self )
+    def is_external(self):
+        return len( self.groups ) < 5
         
 middle = circut.split(":")[1]
 for gate in middle.split(","):
     m = re.match(r"([\d]+)([RL])([\d]+)([RL])0#([\d])+([RL])([\d]+)([RL])", gate )
     if m:
         cgate( gate, m.groups() )
-        print "%20s"%gate, m.groups()
+        print "# %20s"%gate, m.groups()
     else:
         m = re.match("X([\d])+([RL])0#X([\d])+([RL])", gate )
         if m:
             cgate( gate, m.groups() )
-            print "%20s"%gate, m.groups()
+            print "# %20s"%gate, m.groups()
         else:
-            print "%20s"%gate, "no match"
+            print "# %20s"%gate, "no match"
 
 for g in  cgate.all :
-    print "#", g.text, g.groups
-    print 'g%d -> g%s [label="I1 %s"];'%(g.id, g.groups[0], g.groups[1] )
-    print 'g%d -> g%s [label="I2 %s"];'%(g.id, g.groups[2], g.groups[3] )
-    print 'g%d -> g%s [label="o1 %s"];'%(g.id, g.groups[4], g.groups[5] )
-    print 'g%d -> g%s [label="o2 %s"];'%(g.id, g.groups[6], g.groups[7] )
-    
+    if g.is_external():
+        print "# External Gate", g.text, g.groups
+        print 'g%d -> g%s [label="I1 %s"];'%(g.id, g.groups[0], g.groups[1] )
+        print 'g%d -> g%s [label="o1 %s"];'%(g.id, g.groups[2], g.groups[3] )
+    else:
+        print "#", g.text, g.groups
+        print 'g%d -> g%s [label="I1 %s"];'%(g.id, g.groups[0], g.groups[1] )
+        print 'g%d -> g%s [label="I2 %s"];'%(g.id, g.groups[2], g.groups[3] )
+        print 'g%d -> g%s [label="o1 %s"];'%(g.id, g.groups[4], g.groups[5] )
+        print 'g%d -> g%s [label="o2 %s"];'%(g.id, g.groups[6], g.groups[7] )
 
 
 
+
+print "}"
