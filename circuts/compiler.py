27788245c98cc62978d3ec7225b8622335c3942d
George <George.Bullis+git@gmail.com>
2010-06-18 19:15:07 -0700

    fixed compiler bugs, removed some debug print tokens

--- circuts/compiler.py
+++ circuts/compiler.py
@@ -1,7 +1,50 @@
 
-source = """0 -> IE,0R
+samples = [
+"""0 -> IE,0R
 E -> 0L"""
+,
+"""0 -> IE,0R
+E -> 0L"""
+,
+"""0 -> 0L, 1E
+E -> 0R"""
+,
+"""0 -> 1E, 0L
+E -> 0R"""
+,
 
+"""0 -> 1E, 0R
+E -> 0L"""
+,
+
+
+"""0 -> 0R, 1E
+E -> 0L"""
+,
+
+"""0 -> 0L, 1R
+1 -> 1L, 2E
+E -> 0R"""
+,
+
+
+"""0 -> 0L, 1R
+1 -> 2E, 1L 
+E -> 0R"""
+,
+
+"""0 -> 0L, 1L
+1 -> 1R, 2E
+E -> 0R""" 
+]
+
+bad = [
+"""0 -> 0L, 1R
+1 -> 2E, 1R
+E -> 0R"""
+
+
+]
 import re
 
 class ccompilegate:
@@ -29,7 +72,7 @@ class ccompilegate:
         if self.id == 'E':
             self.external = True
             self.id = ccompilegate.get_externalnode()
-            print "Updating E node"
+            #print "Updating E node"
             ccompilegate.allgates[ self.id ] = self
             del ccompilegate.allgates[ 'E' ]
             ccompilegate.externalnode = self.id
@@ -37,12 +80,15 @@ class ccompilegate:
         if self.o1side == "L":
             ccompilegate.allgates[ self.o1node ].input1node = self.id
             ccompilegate.allgates[ self.o1node ].input1side = 'L'
+            if self.external:
+                ccompilegate.allgates[ self.o1node ].input1side = 'R'
+
         if self.o1side == "R":
             ccompilegate.allgates[ self.o1node ].input2node = self.id
             ccompilegate.allgates[ self.o1node ].input2side = 'L'
+            if self.external:
+                ccompilegate.allgates[ self.o1node ].input2side = 'R'
 
-        if self.external:
-            ccompilegate.allgates[ self.o1node ].input1side = 'R'
 
 
         if self.o2side == "L":
@@ -71,45 +117,54 @@ class ccompilegate:
 
 
     
+def compile ( source ):
+    ccompilegate.allgates = {}
 
-x = source.split("\n")
-for inputs in x:
-    m = re.match(r"([\d]+)\s*->\s*([\dI]+)([ERL]),([\d]+)([ERL])", inputs )
-    if m:
-        g = m.groups()
-        print g
-        ccompilegate( g[0], g[1], g[2], g[3], g[4] )
-    else:
-        m = re.match(r"(E)\s*->\s*([\dI]+)([RL])", inputs )
+    x = source.split("\n")
+    for inputs in x:
+        m = re.match(r"([\d]+)\s*->\s*([\dI]+)([ERL])\s*,\s*([\d]+)([ERL])", inputs )
         if m:
             g = m.groups()
-            print g
-            ccompilegate( g[0], g[1], g[2], None, None )
+            #print g
+            ccompilegate( g[0], g[1], g[2], g[3], g[4] )
         else:
-            print inputs, "no match"
-
-c = ccompilegate
+            m = re.match(r"(E)\s*->\s*([\dI]+)([RL])", inputs )
+            if m:
+                g = m.groups()
+                #print g
+                ccompilegate( g[0], g[1], g[2], None, None )
+            else:
+                print inputs, "no match"
     
 
-c.allgates['E'].update_other()
-for i in c.allgates:
-    print "Updating ",i, c.allgates[i].id
-    c.allgates[i].update_other()
+    c = ccompilegate
+        
+    
+    c.allgates['E'].update_other()
+    for i in c.allgates:
+        c.allgates[i].update_other()
+    
+    
+    keys = ccompilegate.allgates.keys()
+    keys.sort()
+    
+    
+    
+    print "%sL:"%(c.get_externalnode())
+    for j in keys:
+        i = ccompilegate.allgates[j]
+        if not i.external:
+            print "%s%s%s%s0#%s%s%s%s,"%( i.input1node, i.input1side, i.input2node, i.input2side, i.o1node, i.o1side, i.o2node, i.o2side )
+    
+    i = c.allgates[ c.get_externalnode() ]
+    print "X%s%s0#X%s%s:"%( i.input1node, i.input1side, i.o1node, i.o1side )
+    
+    
+    print "%sL"%(c.get_externalnode())
 
 
-keys = ccompilegate.allgates.keys()
-keys.sort()
-print keys
+for code in samples:
+    print "================="
+    print code
+    compile( code )
 
-
-print "%sL:"%(c.get_externalnode())
-for j in keys:
-    i = ccompilegate.allgates[j]
-    if not i.external:
-        print "%s:%s%s%s%s0#%s%s%s%s,"%(i.id, i.input1node, i.input1side, i.input2node, i.input2side, i.o1node, i.o1side, i.o2node, i.o2side )
-
-i = c.allgates[ c.get_externalnode() ]
-print "%s:X%s%s0#X%s%s:"%(i.id, i.input1node, i.input1side, i.o1node, i.o1side )
-
-
-print "%sL"%(c.get_externalnode())
